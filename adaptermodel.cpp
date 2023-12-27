4baa3c7672ccd378c2b6d76a9a68bbadabefe604
B. Eschrich (PARI-PACC) <bjoern.eschrich@pari.com>
2023-12-27 05:41:34 +0100

    wip

--- adaptermodel.cpp
+++ adaptermodel.cpp
@@ -13,6 +13,10 @@ AdapterModel::AdapterModel(QObject *parent)
     qDBusRegisterMetaType<InterfaceList>();
     qDBusRegisterMetaType<ManagedObjectList>();
 
+    m_roleNames[AdapterPath] = "path";
+    m_roleNames[AdapterName] = "name";
+    m_roleNames[ItemText] = "itemText";
+
     m_objectManager = new QDBusInterface("org.bluez", "/", "org.freedesktop.DBus.ObjectManager", QDBusConnection::systemBus());
 
     QDBusPendingReply<ManagedObjectList> reply = m_objectManager->call("GetManagedObjects");
@@ -35,14 +39,48 @@ AdapterModel::AdapterModel(QObject *parent)
     }
 }
 
-int AdapterModel::rowCount(const QModelIndex &parent) const
+int AdapterModel::columnCount(const QModelIndex &) const
 {
-    Q_UNUSED(parent)
+    return m_roleNames.count();
+}
+
+QVariant AdapterModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (section) {
+            case 0:
+                return tr("path");
+            case 1:
+                return tr("name");
+            case 2:
+                return tr("itemText");
+        }
+    }
+    return QVariant();
+}
+
+int AdapterModel::rowCount(const QModelIndex &) const
+{
     return m_devices.count();
 }
 
 QVariant AdapterModel::data(const QModelIndex &index, int role) const
 {
+    if (role == Qt::DisplayRole) {
+        switch (index.column()) {
+            case 0: {
+                return data(index, AdapterPath);
+            }
+            case 1: {
+                return data(index, AdapterName);
+            }
+            case 2: {
+                return data(index, ItemText);
+            }
+        }
+        return QVariant();
+    }
+
     if (index.row() >= 0 && index.row() < m_devices.length()) {
         if (role == AdapterPath || role == ItemText) {
             return m_devices.at(index.row());
@@ -50,14 +88,11 @@ QVariant AdapterModel::data(const QModelIndex &index, int role) const
             return m_deviceNames.at(index.row());
         }
     }
+
     return QVariant();
 }
 
 QHash<int, QByteArray> AdapterModel::roleNames() const
 {
-    QHash<int, QByteArray> names;
-    names[AdapterPath] = "path";
-    names[AdapterName] = "name";
-    names[ItemText] = "itemText";
-    return names;
+    return m_roleNames;
 }
--- adaptermodel.h
+++ adaptermodel.h
@@ -12,14 +12,19 @@ public:
     AdapterModel(QObject *parent = nullptr);
     enum Roles { AdapterPath = Qt::UserRole + 1, AdapterName, ItemText };
 
+    QHash<int, QByteArray> roleNames() const override;
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const override;
 
 private:
     QDBusInterface *m_objectManager = nullptr;
     QStringList m_devices;
     QStringList m_deviceNames;
+    QHash<int, QByteArray> m_roleNames;
 };
 
 #endif // ADAPTERMODEL_H
